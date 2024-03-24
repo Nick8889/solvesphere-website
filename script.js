7e5717a05683458d47ea53c1d2c882e2948b9fcc
Nick8889 <78318606+Nick8889@users.noreply.github.com>
2024-03-24 19:41:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.getElementById('get-answers').addEventListener('click', function() {
     document.getElementById('main-content').innerHTML = `
         <header class="site-header">
             <div class="container header-container">
-                <img src="logo.png" alt="SolveSphere Logo" class="logo">
                 <nav class="site-nav">
                     <a href="index.html" class="nav-link">Home</a>
                 </nav>
